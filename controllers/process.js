a788fba865545e37cfaa0e1dc61c29e05d31a762
Max Schulz-Kunowski <max.schulz-kunowski@ey.com>
2018-06-15 14:03:56 -0700

    Support for Track Characteristic Variance control to allow more fine tuning, updated .env

--- controllers/process.js
+++ controllers/process.js
@@ -1,5 +1,9 @@
+require('dotenv').config();
+
 const Request = require('../services').request;
 const _ = require('lodash');
+
+const { ...env } = process.env;
 
 const ProcessController = async request => {
   const seeds = _.get(request, 'body.artists', false);
@@ -32,7 +36,6 @@ async function _requestBatchArtists(artistsTrackCollection) {
       , [])
     )
   });
-
 }
 
 async function _requestBatchTrackStyles(tracksCollection) {
@@ -40,7 +43,8 @@ async function _requestBatchTrackStyles(tracksCollection) {
     params: {
       ids: tracksCollection.join(',')
     }
-  })
+  });
+
   return await req('/audio-features')
     .then((r) => _.get(r, 'data.audio_features', {}), (e) => e.error);
 }
@@ -60,32 +64,45 @@ async function _requestRecommendationsFromVitals(topTracks, recommendationModel,
 }
 
 async function _processVitals(tracksVitals) {
-  const trackCharacteristics = [
-    'acousticness',
-    'danceability',
-    'energy',
-    'instrumentalness',
-    'key',
-    'liveness',
-    'loudness',
-    'speechiness',
-    'tempo',
-    'valence',
-  ];
+  const trackCharacteristics = env.SPOTIFY_TRACK_CHARACTERISTICS.split(',');
+  const trackIgnoreVariances = env.SPOTIFY_TRACK_IGNORE_VARIANCES.split(',');
+  const trackVarianceMax = env.SPOTIFY_TRACK_VARIANCE_MAX > 0 ? env.SPOTIFY_TRACK_VARIANCE_MAX : 0;
 
   return await _.reduce(trackCharacteristics, (res, prop) => {
-    res[`max_${prop}`] = _.maxBy(tracksVitals, (t) => t[prop])[prop];
-    res[`min_${prop}`] = _.minBy(tracksVitals, (t) => t[prop])[prop];
+    let _max = void 0;
+    let _min = void 0;
+
+    _max = _.maxBy(tracksVitals, (t) => t[prop])[prop] * 100;
+    _min = _.minBy(tracksVitals, (t) => t[prop])[prop] * 100;
+
+    if (
+      trackVarianceMax
+      && (trackIgnoreVariances.indexOf(prop) < 0)
+      && (_max - _min > trackVarianceMax)
+    ) {
+      const varianceToModify = ((_max - _min) / 2) - (trackVarianceMax / 2);
+
+      _max = (_max - varianceToModify);
+      _min = (_min + varianceToModify);
+    }
+
+    res[`max_${prop}`] = +(_max / 100).toFixed(4);
+    res[`min_${prop}`] = +(_min / 100).toFixed(4);
+
     return res;
   }, {});
 }
 
 async function _processFinalTrackList(recommendations) {
-  return await !recommendations ? {} : _.chain(recommendations)
+  return await !recommendations ? [] : _.chain(recommendations)
     .get('tracks', [])
     .reduce((res, track) => {
       res.push({
-        trackImage: _.get(track.album, `images[${track.album.images.length - 1}].url`, 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8Xw8AAoMBgDTD2qgAAAAASUVORK5CYII='),
+        trackImage: _.get(
+          track.album,
+          `images[${track.album.images.length - 1}].url`,
+          'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mP8Xw8AAoMBgDTD2qgAAAAASUVORK5CYII='
+        ),
         trackUri: track.uri,
         trackTitle: track.name,
         trackArtist: track.artists.map((artist) => artist.name).join(', '),
